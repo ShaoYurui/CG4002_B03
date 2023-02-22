ff86e2509dbf7775998614d4f282d0787ca14bb1
Yurui <69446663+ShaoYurui@users.noreply.github.com>
2023-02-22 14:43:53 +0800

    Add files via upload

--- Hardware Components/Vest/IR_RECEIVER/IR_RECEIVER.ino
+++ Hardware Components/Vest/IR_RECEIVER/IR_RECEIVER.ino
@@ -19,6 +19,18 @@ int health_pt = 100;
 /////////////////////////////////// LED STRIPS VARIABELS ////////////////////////////////////                                    
 CRGB leds[NUM_LEDS];
 /////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// COMMUNICATION DEFINES ///////////////////////////////////
+#define VEST_DATA              0x05
+#define PLAYER_ID              0x01 //0x01 or 0x02
+#define PAD_BYTE               0x00
+#define REQUEST_H              0x48
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// COMMUNICATION VARIABLES /////////////////////////////////
+int message_id = 0;
+uint8_t ack_h[20] = {0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30, 
+                    0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x40};
+bool handshake_done = false;
+/////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////// MAIN FUNCTION////////////////////////////////////////////
 void setup()  
 {  
@@ -30,6 +42,13 @@ void setup()
                                
 void loop()  
 {  
+  if (Serial.available()) {
+    if (Serial.read() == REQUEST_H) {
+      printHandshakeAck();
+      handshake_done = true;
+    }
+  }
+  
   if (IrReceiver.decode()) {
     if((IrReceiver.decodedIRData.address == PLAYER1_IR_SIGNAL_ADDRESS) && 
       (IrReceiver.decodedIRData.command == PLAYER1_IR_SIGNAL_COMMAND))
@@ -113,4 +132,51 @@ void led_strips_display(int num, int R, int G, int B)
   FastLED.show();
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// COMMUNICATION FUNTIONS DEFINES//////////////////////////
 
+void send_data() {
+  uint8_t package[20];
+  package[19] = (uint8_t) getChecksum();
+  package[18] = (uint8_t) PAD_BYTE;
+  package[17] = (uint8_t) PAD_BYTE;
+  package[16] = (uint8_t) PAD_BYTE;
+  package[15] = (uint8_t) PAD_BYTE;
+  package[14] = (uint8_t) PAD_BYTE;
+  package[13] = (uint8_t) PAD_BYTE;
+  package[12] = (uint8_t) PAD_BYTE;
+  package[11] = (uint8_t) PAD_BYTE;
+  package[10] = (uint8_t) PAD_BYTE;
+  package[9] = (uint8_t) PAD_BYTE;
+  package[8] = (uint8_t) PAD_BYTE;
+  package[7] = (uint8_t) PAD_BYTE;
+  package[6] = (uint8_t) PAD_BYTE;
+  package[5] = (uint8_t) PAD_BYTE;
+  package[4] = (uint8_t) PAD_BYTE;
+  package[3] = (uint8_t) PAD_BYTE;
+  package[2] = (uint8_t) PLAYER_ID;
+  package[1] = (uint8_t) message_id;
+  package[0] = (uint8_t) VEST_DATA;
+
+  Serial.write (package, 20);
+  if (message_id == 0) {
+    message_id = 1;
+  } else {
+    message_id = 0;
+  }
+}
+
+void printHandshakeAck() {
+  for (int i = 0; i < 20; i++) {
+    Serial.write(ack_h[i]);
+  }
+}
+
+uint8_t getChecksum() {
+  uint8_t sum = 0;
+  sum ^= VEST_DATA;
+  sum ^= message_id;
+  sum ^= PLAYER_ID;
+
+  return sum;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
