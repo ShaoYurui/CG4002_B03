0bcfb4e4177b608d30f40abdeba2cbcf576ae705
Yurui <69446663+ShaoYurui@users.noreply.github.com>
2023-02-22 14:44:50 +0800

    Add files via upload

--- Hardware Components/Gun/IR_EMITTER/IR_EMITTER.ino
+++ Hardware Components/Gun/IR_EMITTER/IR_EMITTER.ino
@@ -8,7 +8,17 @@
 #define PLAY1_IR_ADDRES 0x1103
 #define BAUD_RATE 115200
 
+#define GUN_DATA               0x04
+#define PLAYER_ID              0x01 //0x01 or 0x02
+#define PAD_BYTE               0x00
+#define REQUEST_H              0x48
+
 int ammo_count = 6;
+
+int message_id = 0;
+uint8_t ack_h[20] = {0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x30, 
+                    0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x40};
+bool handshake_done = false;
 
 void setup()  
 {  
@@ -20,7 +30,12 @@ void setup()
 }  
 void loop()  
 {  
-
+  if (Serial.available()) {
+    if (Serial.read() == REQUEST_H) {
+      printHandshakeAck();
+      handshake_done = true;
+    }
+  }
 }  
 
 void disable_isr()
@@ -66,13 +81,58 @@ void button_isr()
   
   disable_isr();
   shoot_IR();
-  sound_space_gun();
+  send_data();
+  sound_space_gun();  
   enable_isr();
 }
 
 void shoot_IR()
 {
-  IrSender.sendNEC(PLAY1_IR_ADDRES, PLAY1_IR_SIGNAL, true);
+  IrSender.sendNEC(PLAY1_IR_ADDRES, PLAY1_IR_SIGNAL, true);  
 }
 
+void send_data() {
+  uint8_t package[20];
+  package[19] = (uint8_t) getChecksum();
+  package[18] = (uint8_t) PAD_BYTE;
+  package[17] = (uint8_t) PAD_BYTE;
+  package[16] = (uint8_t) PAD_BYTE;
+  package[15] = (uint8_t) PAD_BYTE;
+  package[14] = (uint8_t) PAD_BYTE;
+  package[13] = (uint8_t) PAD_BYTE;
+  package[12] = (uint8_t) PAD_BYTE;
+  package[11] = (uint8_t) PAD_BYTE;
+  package[10] = (uint8_t) PAD_BYTE;
+  package[9] = (uint8_t) PAD_BYTE;
+  package[8] = (uint8_t) PAD_BYTE;
+  package[7] = (uint8_t) PAD_BYTE;
+  package[6] = (uint8_t) PAD_BYTE;
+  package[5] = (uint8_t) PAD_BYTE;
+  package[4] = (uint8_t) PAD_BYTE;
+  package[3] = (uint8_t) PAD_BYTE;
+  package[2] = (uint8_t) PLAYER_ID;
+  package[1] = (uint8_t) message_id;
+  package[0] = (uint8_t) GUN_DATA;
 
+  Serial.write (package, 20);
+  if (message_id == 0) {
+    message_id = 1;
+  } else {
+    message_id = 0;
+  }
+}
+
+void printHandshakeAck() {
+  for (int i = 0; i < 20; i++) {
+    Serial.write(ack_h[i]);
+  }
+}
+
+uint8_t getChecksum() {
+  uint8_t sum = 0;
+  sum ^= GUN_DATA;
+  sum ^= message_id;
+  sum ^= PLAYER_ID;
+
+  return sum;
+}
